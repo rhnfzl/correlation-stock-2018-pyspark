689a493e0151b2849c213a795c5b1eca71da2b99
Rehan <rehanfazal@live.com>
2021-12-05 10:29:04 +0100

    updating azure settings

---
+++ cloud setup/fix-conda.sh
@@ -0,0 +1,2 @@
+rm /usr/bin/anaconda/lib/python2.7/site-packages/conda/core/subdir*
+wget https://gregorysfixes.blob.core.windows.net/public/subdir_data.py -P /usr/bin/anaconda/lib/python2.7/site-packages/conda/core/
